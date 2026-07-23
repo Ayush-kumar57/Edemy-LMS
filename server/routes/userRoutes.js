70ad020b3eac4cd8ef37f0f8d4ca6159c655ef62
Ayush-kumar57 <ayushkumarlp57707@gmail.com>
2026-07-24 02:10:02 +0530

    add user controllers

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,6 +4,9 @@ import {
   getUserData,
   userEnrolledCourses,
   purchaseCourse,
+  updateUserCourseProgress,
+  getUserCourseProgress,
+  addUserRating,
 } from '../controllers/userController.js';
 import { requireAuth } from '@clerk/express';
 
@@ -12,5 +15,10 @@ const userRouter = express.Router();
 userRouter.get('/data', getUserData);
 userRouter.get('/enrolled-courses', userEnrolledCourses);
 userRouter.post('/purchase', requireAuth(), purchaseCourse);
+
+userRouter.post('/update-course-progress', updateUserCourseProgress)
+userRouter.post('/get-course-progress', getUserCourseProgress)
+userRouter.post('/add-rating', addUserRating)
+
 
 export default userRouter;
